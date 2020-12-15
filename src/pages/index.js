9033f7f569f626a532d0a55ea6ad20b6a82a8e5d
Sebastian <sebastian.chang23@gmail.com>
2020-12-15 10:19:44 -0600

    finished basic blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,65 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
+
+const IndexPage = ({ data }) => {
+  console.log('this is the data', data)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h1>Sebastian's Thoughts</h1>
+        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        {
+          data.allMarkdownRemark.edges.map(({ node }) => {
+            console.log('this is inside node ', node)
+            return(
+            <div key={node.id}>
+              <BlogLink to={node.fields.slug}>
+                <BlogTitle>{node.frontmatter.title} - {node.frontmatter.date}</BlogTitle>
+              </BlogLink>
+              <p>{node.excerpt}</p>
+            </div>
+          )})
+        }
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+query {
+  allMarkdownRemark (sort: {fields: [frontmatter___date], order: DESC}) {
+    totalCount
+    edges {
+      node {
+        id
+        frontmatter {
+          date
+          description
+          title
+        }
+        fields {
+          slug
+        }
+        excerpt(truncate: true)
+      }
+    }
+  }
+}
+`
